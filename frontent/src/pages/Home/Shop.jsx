3d629a8f2d8bc927c06a39de7e5a300df509ed20
Al Ameen S <alameensameen77@gmail.com>
2025-11-26 12:55:34 +0530

    refactored shop page card responsiveness

--- frontent/src/pages/Home/Shop.jsx
+++ frontent/src/pages/Home/Shop.jsx
@@ -227,7 +227,7 @@ const Shop = () => {
 
                     <div className="flex-1">
 
-                        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mb-8">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mb-8">
                             {productData?.length == 0 ? <p>No Products Found</p>
                                 :
                                 (
